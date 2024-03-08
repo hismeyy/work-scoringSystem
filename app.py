9374fd8cc1cf7d4e88bfe506ef13380c6c66dc48
MaxCosmos <renyh2001@outlook.com>
2024-03-08 23:49:29 +0800

    完成大部分

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
+from datetime import datetime
 import os
 import tkinter as tk
 from tkinter import filedialog
+import anytree
 
 import webview
 
 from script.parsing_excel import *
 
 data = None
-operation_data = None
+operation_data = []
 score_data = []
 project_data = []
 result_project_data = []
@@ -21,7 +23,7 @@ g_page_num = -1
 
 
 class Api:
-    def startTest(self, car_model, tester_name, file_path):
+    def startTest(self, car_model, tester_name, file_path, flag):
         global data
         global operation_data
         global score_data
@@ -30,10 +32,25 @@ class Api:
         global g_car_model
         global g_tester_name
         global g_file_path
+        global g_result
+        global g_page_num
 
-        print("车型:", car_model)
-        print("测试员名字:", tester_name)
-        print("文件:", file_path)
+        print(flag)
+        if not flag:
+            # 清空所有的值
+            data = None
+            operation_data = []
+            score_data = []
+            project_data = []
+            result_project_data = []
+            g_result = []
+
+            g_car_model = None
+            g_tester_name = None
+            g_file_path = None
+
+            g_page_num = -1
+
 
         g_car_model = car_model
         g_tester_name = tester_name
@@ -44,6 +61,8 @@ class Api:
         score_data = get_score(data)
         project_data = get_project(data)
         result_project_data = build_data(operation_data, score_data, project_data)
+
+        print(project_data)
 
         obj = {
             "g_page_num": g_page_num,
@@ -77,6 +96,7 @@ class Api:
 
         # 将新的结果对象添加到列表中
         g_result.append(obj)
+        print(g_result)
 
         return g_result
 
@@ -96,6 +116,47 @@ class Api:
             i += 1
         return result_project
 
+    def getScoreProject(self, item):
+        global score_data
+        global project_data
+        temp_data_list = project_data
+        temp_data_list = temp_data_list[len(temp_data_list) - 1:][0]
+        i = 0
+        for temp in temp_data_list:
+            if temp == item:
+                break
+            i = i + 1
+        return i
+
+    def getScorePage(self, index):
+        global score_data
+        result = []
+        for score_list in score_data:
+            result.append(score_list[int(index)])
+        return result
+
+    def setScorp(self, index, i, score):
+        global g_result
+        global operation_data
+        obj = None
+        for temp in g_result:
+            if temp["index"] == int(index):
+                obj = temp  # 如果找到了匹配的index，直接返回该对象
+        if obj is None:
+            # 如果没有找到，创建一个新的对象
+            result_score_list = [None] * len(operation_data[0])
+            result_score_list[int(i)] = int(score)
+            # 构造要添加的新结果对象
+            obj = {
+                "index": int(index),
+                "result_score_list": result_score_list
+            }
+            g_result.append(obj)
+        else:
+            result_score_list = obj["result_score_list"]
+            result_score_list[int(i)] = int(score)
+        return g_result
+
     def getFile(self):
         root = tk.Tk()
         root.withdraw()
@@ -103,6 +164,58 @@ class Api:
         root.quit()
         root.destroy()
         return file_path
+
+    def outScore(self):
+        global g_car_model
+        global g_tester_name
+        global g_file_path
+        global operation_data
+        global g_result
+        root = tk.Tk()
+        root.withdraw()  # 隐藏主窗口
+        folder_path = filedialog.askdirectory()  # 弹出对话框让用户选择文件夹
+        root.quit()
+        root.destroy()
+
+        # 获取当前日期
+        current_date = datetime.now().strftime("%Y.%m.%d")
+
+        # 构建文件名
+        file_name = f"{g_car_model}-{g_tester_name}-{current_date}.xlsx"
+
+        # 开始行数
+        start_row = 0
+        with open('./conf/app.ini', 'r') as f:
+            # 读取第一行
+            first_line = f.readline()
+            # 判断是否包含指定字符串
+            if 'operation_start_row=' in first_line:
+                # 获取等号后的数字部分并转换为整数
+                start_row = int(first_line.split('=')[1])
+            else:
+                print("No operation_start_row found in the first line.")
+
+        max_row = len(operation_data) + start_row # 20
+
+        workbook = openpyxl.load_workbook(g_file_path)
+        sheet = workbook.active
+
+        for i in range(start_row, max_row):
+            for temp_result in g_result:
+                index = int(temp_result["index"])
+                if index == i - start_row:
+                    # 是当前需要的分数值
+                    result_score_list = temp_result["result_score_list"]
+                    result_score_list_size = len(result_score_list)
+                    for lie in range(1, result_score_list_size + 1):
+                        if result_score_list[lie - 1] != 0:
+                            sheet.cell(row=i, column=lie + 1).value = result_score_list[lie - 1]
+                        else:
+                            sheet.cell(row=i, column=lie + 1).value = None
+
+        output_path = os.path.join(folder_path, file_name)
+        # 保存工作簿
+        workbook.save(output_path)
 
 
 if __name__ == '__main__':
--- views/start/main.js
+++ views/start/main.js
@@ -1,4 +1,5 @@
 confirmModal = $('#confirmModal')
+out = $('#out')
 g_result = [] // 得分对象
 g_index = undefined // 选择的第几个操作
 current_project = [] // 当前的打分项目
@@ -10,6 +11,9 @@ self_one = null
 self_two = null
 self_three = null
 
+stark = []
+
+
 // 获取文件
 function getFile() {
     // 调用 Python 端定义的函数
@@ -20,7 +24,26 @@ function getFile() {
 }
 
 // 点击开始
-function startTest() {
+function startTest(flag) {
+    // 清空所有的值
+    if (!flag) {
+        console.log(flag)
+        confirmModal = $('#confirmModal')
+        out = $('#out')
+        g_result = [] // 得分对象
+        g_index = undefined // 选择的第几个操作
+        current_project = [] // 当前的打分项目
+        current_index = -1 // 当前的打分项目索引
+        up_self = null // 用来放回上一层的数据
+        up_data_category = "" // 点击具体打分项目
+
+        self_one = null
+        self_two = null
+        self_three = null
+
+        stark = []
+    }
+
     let carModel = document.getElementById('carModel').value;
     let testerName = document.getElementById('testerName').value;
     let file_path = document.getElementById('fileUpload').innerText;
@@ -30,7 +53,7 @@ function startTest() {
     }
 
     // 设置操作
-    pywebview.api.startTest(carModel, testerName, file_path).then(function (response) {
+    pywebview.api.startTest(carModel, testerName, file_path, flag).then(function (response) {
         // 隐藏box
         document.getElementById('box').style.display = 'none';
 
@@ -59,6 +82,43 @@ function startTest() {
             });
             controlDiv.appendChild(button);
         });
+
+        const button1 = document.createElement('button');
+        button1.textContent = "测试完成";
+        button1.className = 'ope_button';
+        // 绑定点击事件
+        button1.addEventListener('click', function () {
+            // 测试完成
+            console.log("测试完成")
+            out.modal("show");
+        });
+        document.querySelector('.control').appendChild(button1);
+
+        const button2 = document.createElement('button');
+        button2.textContent = "回到主页";
+        button2.className = 'ope_button';
+        // 绑定点击事件
+        button2.addEventListener('click', function () {
+            // 测试完成 回到主页
+            console.log("回到主页")
+
+            // 获取所有 input 类型的元素
+            var inputs = document.querySelectorAll('input[type="text"]');
+
+            // 遍历这些元素，并清空它们的值
+            inputs.forEach(function (input) {
+                input.value = '';
+            });
+
+            // 清空 fileUpload 标签的内容
+            document.getElementById('fileUpload').innerHTML = '';
+
+            document.getElementById('box').style.display = 'block';
+            document.querySelector('.control').style.display = 'none';
+
+
+        });
+        document.querySelector('.control').appendChild(button2);
     })
 }
 
@@ -84,7 +144,7 @@ function no(index) {
     // 所有的等于1的得分项目都为6
     pywebview.api.defaultScore(index).then(function (response) {
         g_result = response
-        startTest()
+        startTest(true)
     })
 }
 
@@ -115,6 +175,7 @@ function yes(index) {
             button.addEventListener('click', function () {
                 clickScoreItem(this, response)
                 self_one = this
+                stark.push(this)
             });
 
             // 将按钮添加到div中
@@ -144,7 +205,66 @@ function yes(index) {
 function clickScoreItem(self, data) {
     up_data_category = data
     if (current_index >= data[0].length - 1) {
-        console.log("最低了")
+        pywebview.api.getScoreProject(self.getAttribute('data-category')).then(function (response1) {
+            console.log(response1)
+            pywebview.api.getScorePage(response1).then(function (response2) {
+                console.log(response2)
+                controlDiv = document.querySelector('.control')
+                controlDiv.innerHTML = ''
+
+                controlDiv.innerHTML += '<h1 style="font-size: 24px">您觉得<span style="color: red"> ' + self.innerText + '</span> 能打几分？</h1>'
+                response2.forEach((value, index) => {
+                    if (value == null) {
+                        value = ''
+                    }
+                    let button = document.createElement('button');
+                    button.className = 'ope_button score_button';
+                    button.textContent = `${index + 1}：${value}`;
+                    button.setAttribute('data-index', index);
+
+                    // 检查g_result数组中是否存在匹配的index
+                    // const isIndexMatched = g_result.some(obj => obj.index === index);
+                    // if (isIndexMatched) {
+                    //     // 如果存在匹配的index，设置按钮的背景颜色为红色
+                    //     button.style.backgroundColor = 'red';
+                    // }
+
+                    button.addEventListener('click', function () {
+                        console.log("按钮点击，索引：", this.getAttribute('data-index'));
+                        console.log("g_index", g_index)
+                        console.log("response1", response1)
+                        console.log("score", index + 1)
+                        pywebview.api.setScorp(g_index, response1, index + 1).then(function (result) {
+                            current_index--
+                            current_index--
+                            console.log(result)
+                            stark.pop()
+                            temp = stark.pop()
+                            clickScoreItem(temp, data)
+                            stark.push(temp)
+
+                        })
+                    });
+                    controlDiv.appendChild(button);
+                });
+
+                // 创建按钮并设置内容
+                const button = document.createElement('button');
+                button.textContent = "回到上一步";
+                button.className = 'ope_button';
+                button.setAttribute('data-category', 1);
+                button.setAttribute('data-index', 1); // 使用局部索引作为示例
+                // 绑定点击事件
+                button.addEventListener('click', function () {
+                    up(self_three, data)
+                });
+                document.querySelector('.control').appendChild(button);
+
+                current_index++
+
+            })
+        })
+
         return
     }
 
@@ -153,8 +273,6 @@ function clickScoreItem(self, data) {
     document.querySelector('.control').innerHTML = '';
     document.querySelector('.control').innerHTML += '<h1 style="font-size: 24px">您觉得' + '<span style="color: red">' + self.getAttribute('data-category') + ' </span> 存在哪些优点或缺点？</h1>'
     // 筛选数据
-    console.log("current_index", current_index)
-    console.log("self", up_self)
     const filteredItems = data.filter(item => item[current_index] === self.getAttribute('data-category'));
     // 提取并去重第二个索引（1索引）的值
     const uniqueCategories = [...new Set(filteredItems.map(item => item[current_index + 1]))];
@@ -166,10 +284,16 @@ function clickScoreItem(self, data) {
         button.setAttribute('data-category', category);
         button.setAttribute('data-index', index); // 使用局部索引作为示例
         temp_self = self_two
+
         // 绑定点击事件
         button.addEventListener('click', function () {
+            self_three = self_two
             clickScoreItem(this, data)
+            console.log(current_index)
+            console.log(data[0].length)
             self_two = this
+            stark.push(this)
+
         });
 
         // 将按钮添加到div中
@@ -185,15 +309,7 @@ function clickScoreItem(self, data) {
     button.setAttribute('data-index', 1); // 使用局部索引作为示例
     // 绑定点击事件
     button.addEventListener('click', function () {
-        console.log("执行选项", current_index)
-        if (current_index <= 2) {
-            console.log("选用第一个", current_index)
-            up(self_one, data)
-        } else {
-            up(temp_self, data)
-        }
-
-
+        up(self_one, data)
     });
     document.querySelector('.control').appendChild(button);
 
@@ -205,9 +321,11 @@ function clickScoreItem(self, data) {
 
 function up(self, data) {
     console.log("up", current_index)
+    stark.pop()
+
     if (current_index === 0) {
         current_index--
-        startTest()
+        startTest(true)
         return
     }
 
@@ -221,10 +339,47 @@ function up(self, data) {
     if (current_index >= 2) {
         current_index--
         current_index--
-        // console.log(this.getAttribute('data-category'))
-        clickScoreItem(self, data)
+        temp = stark.pop()
+        clickScoreItem(temp, data)
+        stark.push(temp)
 
     }
+}
+
+
+document.getElementById('outYes').addEventListener('click', function () {
+    outYes();
+});
+
+// 选择否
+document.getElementById('outNo').addEventListener('click', function () {
+    out.modal('hide'); // 关闭模态框
+});
+
+
+// 打印结果
+function outYes() {
+    console.log("打印结果")
+    pywebview.api.outScore().then(function (response) {
+
+
+        out.modal('hide'); // 关闭模态框
+
+        // 获取所有 input 类型的元素
+        var inputs = document.querySelectorAll('input[type="text"]');
+
+        // 遍历这些元素，并清空它们的值
+        inputs.forEach(function (input) {
+            input.value = '';
+        });
+
+        // 清空 fileUpload 标签的内容
+        document.getElementById('fileUpload').innerHTML = '';
+
+        document.getElementById('box').style.display = 'block';
+        document.querySelector('.control').style.display = 'none';
+    })
+
 }
 
 
